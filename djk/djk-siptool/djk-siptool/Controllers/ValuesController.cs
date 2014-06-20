1761daa2ff7f2234e7665611ef905c50cca65692
Mikkel Madsen <m@madsn.net>
2014-06-20 18:58:45 +0200

    DJK web api

---
+++ djk/djk-siptool/djk-siptool/Controllers/ValuesController.cs
@@ -0,0 +1,102 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace djk_siptool.Controllers
+{
+    public class ValuesController : ApiController
+    {
+
+        // GET api/values/45643123
+        public HttpResponseMessage Get(String id)
+        {
+            HttpResponseMessage response;
+            try
+            {
+                SqlConnection cnn = getSqlConn();
+                SqlCommand cmd = getQueryCommand(id, cnn);
+                cnn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        var r = Serialize(reader);
+                        string json = JsonConvert.SerializeObject(r, Formatting.Indented);
+                        response = Request.CreateResponse(HttpStatusCode.OK, json);
+                    }
+                    else
+                    {
+                        response = new HttpResponseMessage(HttpStatusCode.NoContent);
+                    }
+                }
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                response = Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message.ToString());
+            }
+            return response;
+        }
+
+        public IEnumerable<Dictionary<string, object>> Serialize(SqlDataReader reader)
+        {
+            var results = new List<Dictionary<string, object>>();
+            var cols = new List<string>();
+            for (var i = 0; i < reader.FieldCount; i++)
+                cols.Add(reader.GetName(i));
+
+            while (reader.Read())
+                results.Add(SerializeRow(cols, reader));
+
+            return results;
+        }
+        private Dictionary<string, object> SerializeRow(IEnumerable<string> cols,
+                                                        SqlDataReader reader)
+        {
+            var result = new Dictionary<string, object>();
+            foreach (var col in cols)
+                result.Add(col, reader[col]);
+            return result;
+        }
+
+        private SqlCommand getQueryCommand(String callerId, SqlConnection cnn)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cnn;
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = "SELECT TOP 10 dbo.tbl_svm_call_events.fld_timestamp, dbo.tbl_svm_calls.fld_callerid, dbo.tbl_svm_consoles.fld_name AS AcdExt, " +
+                        "dbo.tbl_svm_users.fld_name AS Username " +
+                        "FROM dbo.tbl_svm_call_events INNER JOIN " +
+                        "dbo.tbl_svm_calls ON dbo.tbl_svm_call_events.fld_call = dbo.tbl_svm_calls.id INNER JOIN " +
+                        "dbo.tbl_svm_consoles ON dbo.tbl_svm_call_events.fld_console = dbo.tbl_svm_consoles.guid INNER JOIN " +
+                        "dbo.tbl_svm_users ON dbo.tbl_svm_call_events.fld_user = dbo.tbl_svm_users.guid " +
+                        "WHERE dbo.tbl_svm_call_events.fld_event = '12' AND dbo.tbl_svm_calls.fld_callerid = @callerid " +
+                        "ORDER BY dbo.tbl_svm_call_events.fld_timestamp DESC";
+            cmd.Parameters.Add("@callerid", System.Data.SqlDbType.VarChar, 20).Value = callerId;
+            return cmd;
+        }
+
+        private SqlConnection getSqlConn()
+        {
+            string connectionString = null;
+            SqlConnection cnn;
+            connectionString = "server=Odin\\SQLEXPRESS;" +
+                "Trusted_Connection=yes;" +
+                "database=testdb; " +
+                "connection timeout=2";
+            /*
+             * "server=Odin\\SQLEXPRESS;" +
+            "Trusted_Connection=yes;" +
+            "database=testdb; " +
+            "connection timeout=30";
+             * */
+            cnn = new SqlConnection(connectionString);
+            return cnn;
+        }
+    }
+}
---
+++ djk/djk-siptool/djk-siptool/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("djk_siptool")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("djk_siptool")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("07e3106e-af6d-43fa-82b4-cc14bec8f161")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
