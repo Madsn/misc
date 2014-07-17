6bfa83088d707798119839e7f634ea5ecb7fa964
Mikkel Madsen <m@madsn.net>
2014-07-17 20:18:27 +0200

    yo angular

---
+++ goinstant/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name goinstantApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the goinstantApp
+ */
+angular.module('goinstantApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
